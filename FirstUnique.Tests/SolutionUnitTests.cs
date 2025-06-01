85bbc3efc21392bc6bff031551a55c2b5b5e1888
Sabri Khodja <sabri.khodja@gmail.com>
2025-06-02 01:52:40 +0200

    Implement 1st unit test

--- FirstUnique.Tests/SolutionUnitTests.cs
+++ FirstUnique.Tests/SolutionUnitTests.cs
@@ -1,7 +1,18 @@
-﻿namespace SolutionTests
+﻿using FirstUnique;
+
+namespace SolutionTests
 {
     public class SolutionUnitTests
     {
-
+        [Theory]
+        [InlineData(new int[] { 4, 10, 5, 4, 2, 10 }, 5)]
+        [InlineData(new int[] { 1, 4, 3, 3, 1, 2 }, 4)]
+        [InlineData(new int[] { 6, 4, 4, 6 }, -1)]
+        public void GivenArray_WhenSolutionCalled_ThenReturnsFirstUniqueInteger(int[] input, int expected)
+        {
+            Solution sol = new Solution();
+            int actual = sol.solution(input);
+            Assert.Equal(expected, actual);
+        }
     }
 }
