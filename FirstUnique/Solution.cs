559d3f4fe915395b49f54e3d1415b5a9f7679c55
Sabri Khodja <sabri.khodja@gmail.com>
2025-06-02 01:58:51 +0200

    Implement the solution() method

--- FirstUnique/Solution.cs
+++ FirstUnique/Solution.cs
@@ -1,7 +1,31 @@
-﻿namespace FirstUnique
+﻿
+namespace FirstUnique
 {
     public class Solution
     {
+        public int solution(int[] input)
+        {
+            HashSet<int> all = new HashSet<int>();
+            HashSet<int> duplicates = new HashSet<int>();
 
+            foreach(int number in input)
+            {
+                if (!all.Add(number))
+                {
+                    duplicates.Add(number);
+                }
+            }
+
+            foreach (int number in input)
+            {
+                if (!duplicates.Contains(number))
+                {
+                    return number;
+                }
+            }
+
+            return -1;
+
+        }
     }
 }
